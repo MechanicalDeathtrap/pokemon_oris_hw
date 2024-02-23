2e5a6b66e31fb66d8aaa87f773b5fe726fe7c835
MechanicalDeathtrap <korikova-04@mail.ru>
2024-02-23 22:32:00 +0300

    rewritten search function

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,35 +1,53 @@
 import styles from './Header.module.sass'
-import {useState} from "react";
-import useStore from "./MainContent.jsx";
-/*import pokemonsProps from "./MainContent.jsx"*/
+import {useEffect, useState} from "react";
+import {usePokemonStore} from "./PokemonsStateStore.jsx"
+import {useSearchedPokemonStore} from "./SearchedStateStore.js";
 
 function Header(){
 
     const [searchInput, setSearchInput] = useState('');
-    const pokemons = useStore((state) => state.pokemonList);
-    console.log(pokemons);
+    const [searchResult, setResult] = useState([]);
+    const {pokemonsList} = usePokemonStore();
+    const {setSearchedList, searchedPokemonsList} = useSearchedPokemonStore()
 
+    useEffect(() => {
+        if (searchInput.length !== 0) {
+            /*        if (searchInput.length != 0 ) {*/
+            const filiteredPokemons = pokemonsList.filter((item) => {
+                console.log((item.pokemon.name).includes(searchInput.toLowerCase()), item.pokemon.name, searchInput);
+                return (item.pokemon.name).toLowerCase().includes(searchInput.toLowerCase())
+            });
+            console.log(filiteredPokemons);
+            setResult(filiteredPokemons);
+        }
+        else{
+            setResult(pokemonsList);
+        }
+    }, [searchInput])
 
-/*    const pokemons = pokemonsProps();*/
-    const searchPokemons = (searchValue) =>{
+    useEffect(() =>{
+        setSearchedList(searchResult);
+        console.log(searchResult);
+    }, [searchResult])
 
-        console.log(pokemons);
-        setSearchInput(searchValue)
-        pokemons.filter((item) => {
-            return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
-        })
+/*    useEffect(() =>{
+        console.log(searchedPokemonsList);
+    }, [searchedPokemonsList])*/
+
+    const searchPokemons = (value) =>{
+        setSearchInput(value)
     }
 
     return(
         <header className={styles.header}>
             <div className={styles.headerContainer}>
-                <img src="src/assets/pokeball.png" alt="poke-ball" className={styles.image}/> {/*PUT AN IMAGE*/}
+                <img src="src/assets/pokeball.png" alt="poke-ball" className={styles.image}/>
                 <h1 className={styles.title}>Who are you looking for?</h1>
                 <div className={styles.inputBox}>
                     <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.svg}>
                         <path d="M8.57621 16.99C10.4275 16.99 12.1561 16.3935 13.5613 15.3993L18.8476 20.6355C19.0929 20.8785 19.4164 21 19.7509 21C20.4758 21 21 20.4477 21 19.7407C21 19.4093 20.8885 19.0999 20.6431 18.8569L15.3903 13.6428C16.4944 12.2067 17.1524 10.4282 17.1524 8.495C17.1524 3.8222 13.2937 0 8.57621 0C3.84758 0 0 3.8222 0 8.495C0 13.1678 3.84758 16.99 8.57621 16.99ZM8.57621 15.1562C4.88476 15.1562 1.8513 12.1405 1.8513 8.495C1.8513 4.84955 4.88476 1.83377 8.57621 1.83377C12.2565 1.83377 15.3011 4.84955 15.3011 8.495C15.3011 12.1405 12.2565 15.1562 8.57621 15.1562Z" fill="grey"/>
                     </svg>
-                    <input type="search" placeholder={"E.g. Pikachu"} className={styles.input} onChange={(e) => searchPokemons(e.target.value)}/>
+                    <input type="search" placeholder={"E.g. Pikachu"} value={searchInput} className={styles.input} onChange={(e) => searchPokemons(e.target.value)}/>
                     <button className={styles.button}>GO</button>
                 </div>
             </div>
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,64 +1,66 @@
-import {create} from 'zustand'
 import style from './MainContainer.module.sass'
 import Abilities from "./Abilities.jsx";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
+import {usePokemonStore} from "./PokemonsStateStore.jsx"
+import {useSearchedPokemonStore} from "./SearchedStateStore.js";
 
+function MainContent() {
 
+    const {pokemonsList, setPokemonsList, refreshPokemonsList} = usePokemonStore();
+    const {searchedPokemonsList, setSearchedList} = useSearchedPokemonStore();
 
-function MainContent(){
-
-    const [pokemons, setPokemons] = useState([]);
-/*
-    const pokemonsProps = () =>{
-        return pokemons
-    }
-*/
-    const useStore = create((set)=> ({
-        pokemonList: [],
-        setPokemonsList: (pokemons) => set(() => ({pokemonList: pokemons})
-        )
-    }));
-
-    const addPokemon = useStore((state) => state.setPokemonsList)
     const getPokemons = async () => {
-        await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=2`)
+        await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=100`)
             .then(response => response.json())
             .then(data =>
                 data.results.forEach(async (item) => {
 
                     const fetchData = await fetch(item.url);
                     const json = await fetchData.json();
-                    setPokemons((list) => [...list, json])
+                    setPokemonsList(json);
                 })
             )
-        addPokemon(pokemons)
 
     }
     useEffect(() => {
+        if (pokemonsList.length > 0) {
+            refreshPokemonsList();
+        }
         getPokemons();
     }, []);
 
+    useEffect(() =>{
+        setSearchedList(pokemonsList)
+    }, [pokemonsList])
 
-    if (pokemons.length > 0){
-
-    return(
-        <div className={style.mainContainer}>
-            { pokemons.map(pokemon  =>(
-                <ul key={pokemon.id} className={style.element}>
-                    <div className={style.info}>
-                        <span className={style.name}>{pokemon.name}</span>
-                        <span>#{pokemon.id}</span>
-                    </div>
-                    <img src={pokemon.sprites.front_default} alt="pokemon" className={style.image}/>
-                    <li className={style.abilitiesList}>
-                        <Abilities property={pokemon.types}/>
-                    </li>
-                </ul>)
-            )
-        }
-        </div>
-    )}
+    if (searchedPokemonsList.length > 0) {
+        return (
+            <div className={style.mainContainer}>
+                {searchedPokemonsList.map(pokemon => (
+                    <ul key={pokemon.pokemon.id} className={style.element}>
+                        <div className={style.info}>
+                            <span className={style.name}>{pokemon.pokemon.name}</span>
+                            <span>#{pokemon.pokemon.id}</span>
+                        </div>
+                        <img src={pokemon.pokemon.sprites.front_default} alt="pokemon" className={style.image}/>
+                        <li className={style.abilitiesList}>
+                            <Abilities property={pokemon.pokemon.types}/>
+                        </li>
+                    </ul>)
+                )
+                }
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className={style.emptySearchContainer}>
+                <h1>Oops! Try again.</h1>
+                <span>The Pokemon you're looking for is a unicorn. It doesn't exist in this list.</span>
+                <img src="src/assets/pikachy.png" alt="pikachu" className={style.emptySearchImage}/>
+            </div>
+        )
+    }
 }
-
 
 export default MainContent
